3c587f731e84d98153465f3e994f5b41b70d48b7
ZuxRoy <snehashis.roysr@gmail.com>
2024-05-14 13:06:59 +0530

    Log file initiation

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main () {
+    logger := getLogger("/home/zuxroy/Code/Projects/lsp-from-scratch/logs.txt")
+    logger.Println("LSP logging initiated")
+}
+
+func getLogger(filename string) *log.Logger {
+    logfile, err := os.OpenFile(filename, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+
+    if err != nil {
+        panic("Error with the log file")
+    }
+
+    return log.New(logfile, "[START] ", log.Ldate|log.Ltime|log.Lshortfile)
+}
